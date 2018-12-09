809e8024db8a7ae2a236c67551b96681bf09dcec
yanshil <yanshi.luo@rutgers.edu>
2018-12-09 15:08:43 -0500

    .

--- getStarted.js
+++ getStarted.js
@@ -5,9 +5,12 @@ scene.background = new THREE.Color( 0x8FBCD4 );
 scene.add( new THREE.AmbientLight( 0x8FBCD4, 0.4 ) );
 
 camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.x = 3;
+camera.position.y = 3;
 camera.position.z = 5;
 scene.add( camera );
 
+// OR http://davidscottlyons.com/threejs-intro/#slide-40
 container = document.getElementById( 'container' );
 var controls = new THREE.OrbitControls( camera, container );
 
@@ -23,7 +26,7 @@ container.appendChild( renderer.domElement );
 // Event listener: resize window
 window.addEventListener( 'resize', onWindowResize, false );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var geometry = new THREE.BoxGeometry( 3, 1, 5, 3, 1, 5 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
@@ -32,7 +35,7 @@ var options = {
     velx: 0,
     vely: 0,
     camera: {
-    speed: 0.0001
+        speed: 0.0001
     },
     stop: function() {
     this.velx = 0;
@@ -42,8 +45,8 @@ var options = {
     this.velx = 0.01;
     this.vely = 0.01;
     camera.position.z = 5;
-    camera.position.x = 0;
-    camera.position.y = 0;
+    camera.position.x = 3;
+    camera.position.y = 3;
     cube.scale.x = 1;
     cube.scale.y = 1;
     cube.scale.z = 1;
@@ -53,19 +56,26 @@ var options = {
 
 // DAT.GUI Related Stuff
 
+//!!!  It is unable to update geometry of mesh by a geometry object once used.
+
 var gui = new dat.GUI();
 
 var box = gui.addFolder('Cube');
+// Only for demo
 box.add(cube.scale, 'x', 0, 10).name('Width').listen();
 box.add(cube.scale, 'y', 0, 10).name('Height').listen();
 box.add(cube.scale, 'z', 0, 10).name('Depth').listen();
+// CANNOT USE
+// box.add(cube.geometry.parameters, 'width', 0, 10).name('Width').listen();
+// box.add(cube.geometry.parameters, 'widthSegments', 1, 10).name('widthSegments').listen();
 box.add(cube.material, 'wireframe').listen();
+// cube.geometry.parameters.widthSegments
 box.open();
 
 var velocity = gui.addFolder('Velocity');
 velocity.add(options, 'velx', -0.2, 0.2).name('X').listen();
 velocity.add(options, 'vely', -0.2, 0.2).name('Y').listen();
-velocity.open();
+velocity.close();
 
 gui.add(options, 'stop');
 gui.add(options, 'reset');
--- notes.md
+++ notes.md
@@ -4,3 +4,9 @@
 * webgl_interactive_draggablecubes.html
 * https://codepen.io/programking/pen/MyOQpO
 * http://davidscottlyons.com/threejs-intro/#slide-45
+* https://threejs.org/docs/#api/en/geometries/CylinderGeometry
+
+
+
+## QA
+* https://stackoverflow.com/questions/25766981/changing-a-three-js-objects-geometry
